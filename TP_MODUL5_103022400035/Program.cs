7918099025cba8317087abacf3c687cf7ba2d2c3
petraaps <petrasimangunsong07@gmail.com>
2026-03-29 21:43:32 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TP_MODUL5_103022400035/Program.cs
+++ TP_MODUL5_103022400035/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ModulGeneric
+{ 
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T input)
+        {
+            Console.WriteLine($"Halo user {input}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+
+            string namaPraktikan = "Petra"; 
+            halo.SapaUser(namaPraktikan);
+        }
+    }
+}
