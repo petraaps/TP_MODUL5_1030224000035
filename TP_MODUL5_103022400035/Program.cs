cf35097a716f28130bf73399412a67bb1a95939a
petraaps <petrasimangunsong07@gmail.com>
2026-03-29 21:53:13 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400035/Program.cs
+++ TP_MODUL5_103022400035/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ModulGeneric
+{
+    public class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nimPraktikan = "103022400035";
+            DataGeneric<string> dataNim = new DataGeneric<string>(nimPraktikan);
+
+            dataNim.PrintData();
+        }
+    }
+}
